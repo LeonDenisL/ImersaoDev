4fadc9167c68af08a379dd89d7cf482efd446895
Leon Denis Lucena <81835127+LeonDenisL@users.noreply.github.com>
2021-12-18 13:40:14 -0300

    Add files via upload

---
+++ super-trunfo-pokemon-sinnoh/README.markdown
@@ -0,0 +1,5 @@
+# Super Trunfo  Pokemon Sinnoh melhorado
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/LeonDenisL/pen/RwKKBje](https://codepen.io/LeonDenisL/pen/RwKKBje).
+
+
---
+++ super-trunfo-pokemon-sinnoh/dist/script.js
@@ -0,0 +1,538 @@
+var cartaA1 = {
+  nome:"Aerus Draconeon",
+  imagem:"http://4.bp.blogspot.com/--bn_Q6X-br0/UtFXy2XBQzI/AAAAAAAAajs/5iNb3CYl-wM/s1600/A1_Super_Trunfo_Pok%C3%A9mon_AerusDraconeon.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 1000,
+    Defense: 600,
+    SpAttack: 500,
+    SpDefense: 500,
+    Speed: 950
+}
+}
+
+var cartaA2 = {
+  nome:"Watt Fuarrint",
+  imagem:"http://2.bp.blogspot.com/-LmHajFY1_78/UtFXzjnhmWI/AAAAAAAAajw/iGpJtj7RJlA/s1600/A2_Super_Trunfo_Pok%C3%A9mon_WattFuarrint.jpg",
+  atributos:{
+    HP: 600,
+    Attack: 500,
+    Defense: 500,
+    SpAttack: 800,
+    SpDefense: 600,
+    Speed: 900
+}
+}
+
+var cartaA3 = {
+  nome:"Titânia",
+  imagem:"http://1.bp.blogspot.com/-iX2Y5cRZZN0/UtFXz0aBBoI/AAAAAAAAaj4/8q21Ha2Y4Jk/s1600/A3_Super_Trunfo_Pok%C3%A9mon_Tit%C3%A2nia.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 950,
+    Defense: 1000,
+    SpAttack: 500,
+    SpDefense: 500,
+    Speed: 600
+}
+}
+
+var cartaA4 = {
+  nome:"Milena",
+  imagem:"http://4.bp.blogspot.com/-qHtIqJUzTxs/UtFX1sfDxXI/AAAAAAAAakE/wzQIQHf9JMw/s1600/A4_Super_Trunfo_Pok%C3%A9mon_Milena.jpg",
+  atributos:{
+    HP: 800,
+    Attack: 500,
+    Defense: 500,
+    SpAttack: 700,
+    SpDefense: 1000,
+    Speed: 600
+}
+}
+
+var cartaA5 = {
+  nome:"Mikau",
+    imagem:"http://1.bp.blogspot.com/-841j9kNQKpU/UtFX2lKvL9I/AAAAAAAAakM/xtPFDojnQI4/s1600/A5_Super_Trunfo_Pok%C3%A9mon_Mikau.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 700,
+    Defense: 700,
+    SpAttack: 900,
+    SpDefense: 700,
+    Speed: 800
+}
+}
+
+var cartaA6 = {
+  nome:"Al Capone",
+  imagem:"http://3.bp.blogspot.com/-lFfuIaoDwIA/UtFX30awdQI/AAAAAAAAakU/Xb9rPpFDsG0/s1600/A6_Super_Trunfo_Pok%C3%A9mon_AlCapone.jpg",
+  atributos:{
+    HP: 900,
+    Attack: 900,
+    Defense: 500,
+    SpAttack: 700,
+    SpDefense: 500,
+    Speed: 600
+}
+}
+
+var cartaA7 = {
+  nome:"Eva",
+  imagem:"http://2.bp.blogspot.com/-tbHew-ve5H4/UtFX5hnjnPI/AAAAAAAAakc/LpvyTinJtvQ/s1600/A7_Super_Trunfo_Pok%C3%A9mon_Eva.jpg",
+  atributos:{
+    HP: 600,
+    Attack: 300,
+    Defense: 300,
+    SpAttack: 1000,
+    SpDefense: 800,
+    Speed: 800
+}
+}
+
+var cartaA8 = {
+  nome:"Chaud",
+  imagem:"http://3.bp.blogspot.com/-msTgTJcYjOE/UtFX63ITD0I/AAAAAAAAakk/juVd58kLi9I/s1600/A8_Super_Trunfo_Pok%C3%A9mon_Chaud.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 650,
+    Defense: 1000,
+    SpAttack: 200,
+    SpDefense: 1000,
+    Speed: 300
+}
+}
+
+var cartaB1 = {
+  nome:"General Castelo Branco",
+  imagem:"http://3.bp.blogspot.com/-w4z8AYgIRIw/UtFZcFfw1OI/AAAAAAAAakw/oau_GqUTG0I/s1600/B1_Super_Trunfo_Pok%C3%A9mon_General.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 800,
+    Defense: 900,
+    SpAttack: 300,
+    SpDefense: 900,
+    Speed: 500
+}
+}
+
+var cartaB2 = {
+  nome:"Glaciallis",
+  imagem:"http://4.bp.blogspot.com/-kssVKJvcV88/UtFZeIUsZdI/AAAAAAAAak8/RP6YqSCx8JA/s1600/B2_Super_Trunfo_Pok%C3%A9mon_Glaciallis.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 300,
+    Defense: 300,
+    SpAttack:700 ,
+    SpDefense: 300,
+    Speed: 900
+}
+}
+
+var cartaB3 = {
+  nome:"Lyndis",
+  imagem:"http://2.bp.blogspot.com/-cgbaRu44Dyg/UtFZd-89fII/AAAAAAAAak4/n7pnHjIwXw0/s1600/B3_Super_Trunfo_Pok%C3%A9mon_Lyndis.jpg",
+  atributos:{
+    HP: 550,
+    Attack: 800,
+    Defense: 500,
+    SpAttack: 800,
+    SpDefense: 500,
+    Speed: 800
+}
+}
+
+var cartaB4 = {
+  nome:"Karl",
+  imagem:"http://4.bp.blogspot.com/-nr0eYfoLiZE/UtFZfCp4jCI/AAAAAAAAalI/Cm5C6ouvJsI/s1600/B4_Super_Trunfo_Pok%C3%A9mon_Karl.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 300,
+    Defense: 300,
+    SpAttack: 800,
+    SpDefense: 750,
+    Speed: 650
+}
+}
+
+var cartaB5 = {
+  nome:"Sophie",
+  imagem:"http://1.bp.blogspot.com/-Y4O72G-w5b4/UtFZhT47L9I/AAAAAAAAalQ/YuIrgAGjYZE/s1600/B5_Super_Trunfo_Pok%C3%A9mon_Sophie.jpg",
+  atributos:{
+    HP: 600,
+    Attack: 400,
+    Defense: 400,
+    SpAttack: 700,
+    SpDefense: 700,
+    Speed: 600
+}
+}
+
+var cartaB6 = {
+  nome:"Marco Polo",
+  imagem:"http://2.bp.blogspot.com/-AYL2QtZWpqw/UtFZif9XMBI/AAAAAAAAalY/XMFBypNPsVo/s1600/B6_Super_Trunfo_Pok%C3%A9mon_MarcoPolo.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 600,
+    Defense: 500,
+    SpAttack: 600,
+    SpDefense: 500,
+    Speed: 550
+}
+}
+
+var cartaB7 = {
+  nome:"Wiki & Mozilla",
+  imagem:"http://3.bp.blogspot.com/-Nt_gUZygMA0/UtFZjh2jerI/AAAAAAAAalg/U_gO22IkVqo/s1600/B7_Super_Trunfo_Pok%C3%A9mon_WikiMozilla.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 400,
+    Defense: 400,
+    SpAttack: 1000,
+    SpDefense: 400,
+    Speed: 800
+}
+}
+
+var cartaB8 = {
+  nome:"Vista",
+  imagem:"http://4.bp.blogspot.com/-aCo7A0ER4Qo/UtFZk01DCFI/AAAAAAAAalo/JEGAGInaLI0/s1600/B8_Super_Trunfo_Pok%C3%A9mon_Vista.jpg",
+  atributos:{
+    HP: 800,
+    Attack: 1000,
+    Defense: 900,
+    SpAttack: 700,
+    SpDefense: 700,
+    Speed: 500
+}
+}
+
+var cartaC1 = {
+  nome:"Panetto",
+  imagem:"http://4.bp.blogspot.com/-cKIURdb1aKc/UtFagRayruI/AAAAAAAAal0/2uX9Y9lxoak/s1600/C1_Super_Trunfo_Pok%C3%A9mon_Panetto.jpg",
+  atributos:{
+    HP: 900,
+    Attack: 500,
+    Defense: 500,
+    SpAttack: 600,
+    SpDefense: 500,
+    Speed: 300
+}
+}
+
+var cartaC2 = {
+  nome:"Akebia",
+  imagem:"http://3.bp.blogspot.com/-ZE9agEeoL1w/UtFaiLVCYoI/AAAAAAAAal8/k-CLlH1p2Ps/s1600/C2_Super_Trunfo_Pok%C3%A9mon_Akebia.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 400,
+    Defense: 300,
+    SpAttack: 900,
+    SpDefense: 750,
+    Speed: 800
+}
+}
+
+var cartaC3 = {
+  nome:"Coffey",
+  imagem:"http://2.bp.blogspot.com/-5dCxWCntdis/UtFaiSOOP7I/AAAAAAAAamA/rsMer06UW5M/s1600/C3_Super_Trunfo_Pok%C3%A9mon_Coffey.jpg",
+  atributos:{
+    HP: 900,
+    Attack: 950,
+    Defense: 950,
+    SpAttack: 200,
+    SpDefense: 200,
+    Speed: 100
+}
+}
+
+var cartaC4 = {
+  nome:"Yoshiki",
+  imagem:"http://1.bp.blogspot.com/-jdC-GzVWasQ/UtFakEnburI/AAAAAAAAamM/gRJhuOvFUQY/s1600/C4_Super_Trunfo_Pok%C3%A9mon_Yoshiki.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 850,
+    Defense: 550,
+    SpAttack: 600,
+    SpDefense: 550,
+    Speed: 700
+}
+}
+
+var cartaC5 = {
+  nome:"Jade",
+  imagem:"http://4.bp.blogspot.com/-0kQyOtCXbr0/UtFal8yrv5I/AAAAAAAAamU/90-wxeSU92I/s1600/C5_Super_Trunfo_Pok%C3%A9mon_Jade.jpg",
+  atributos:{
+    HP: 600,
+    Attack: 400,
+    Defense: 500,
+    SpAttack: 800,
+    SpDefense: 300,
+    Speed: 800
+}
+}
+
+var cartaC6 = {
+  nome:"Tom Sawyer",
+  imagem:"http://2.bp.blogspot.com/-u46yPOQcC9s/UtFanUGB1NI/AAAAAAAAamc/vzBHynYAk3k/s1600/C6_Super_Trunfo_Pok%C3%A9mon_TomSawyer.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 800,
+    Defense: 600,
+    SpAttack: 800,
+    SpDefense: 600,
+    Speed: 700
+}
+}
+
+var cartaC7 = {
+  nome:"Beliel",
+  imagem:"http://3.bp.blogspot.com/-BUKIgt_xiWI/UtFaonaWW_I/AAAAAAAAamk/AjjDxW4GPfM/s1600/C7_Super_Trunfo_Pok%C3%A9mon_Beliel.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 700,
+    Defense: 500,
+    SpAttack: 900,
+    SpDefense: 650,
+    Speed: 800
+}
+}
+
+var cartaC8 = {
+  nome:"Seth",
+  imagem:"http://3.bp.blogspot.com/-0Fzm95Uz5MY/UtFaqDbbs_I/AAAAAAAAams/loadQf3gTTE/s1600/C8_Super_Trunfo_Pok%C3%A9mon_Seth.jpg",
+  atributos:{
+    HP: 800,
+    Attack: 1000,
+    Defense: 800,
+    SpAttack: 700,
+    SpDefense: 800,
+    Speed: 800
+}
+}
+
+var cartaD1 = {
+  nome:"Paola",
+  imagem:"http://1.bp.blogspot.com/--fO2sCiTcGk/UtFbH4NC7BI/AAAAAAAAam4/eStl6eMt3L0/s1600/D1_Super_Trunfo_Pok%25C3%25A9mon_Paula.jpg",
+  atributos:{
+    HP: 800,
+    Attack: 900,
+    Defense: 800,
+    SpAttack: 1000,
+    SpDefense: 900,
+    Speed: 800
+}
+}
+
+var cartaD2 = {
+  nome:"Duke",
+  imagem:"http://1.bp.blogspot.com/-DQvaSp0gB48/UtFbJ8pEIJI/AAAAAAAAanE/eEIOFE0tD-0/s1600/D2_Super_Trunfo_Pok%25C3%25A9mon_Duke.jpg",
+  atributos:{
+    HP: 200,
+    Attack: 200,
+    Defense: 200,
+    SpAttack: 300,
+    SpDefense: 200,
+    Speed: 200
+}
+}
+
+var cartaD3 = {
+  nome:"Milady",
+  imagem:"http://4.bp.blogspot.com/-qiArhrZcA2k/UtFbKc155aI/AAAAAAAAanI/Fbw6o9lDUSQ/s1600/D3_Super_Trunfo_Pok%25C3%25A9mon_Milady.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 500,
+    Defense: 750,
+    SpAttack: 800,
+    SpDefense: 700,
+    Speed: 500
+}
+}
+
+var cartaD4 = {
+  nome:"Isaac",
+  imagem:"http://1.bp.blogspot.com/-XgwOgItcXFs/UtFbMKi3InI/AAAAAAAAanQ/7sgbDcO67uo/s1600/D4_Super_Trunfo_Pok%25C3%25A9mon_Isaac.jpg",
+  atributos:{
+    HP: 600,
+    Attack: 750,
+    Defense: 800,
+    SpAttack: 500,
+    SpDefense: 600,
+    Speed: 600
+}
+}
+
+var cartaD5 = {
+  nome:"Sly Stallone - The Blocky",
+  imagem:"http://3.bp.blogspot.com/-tqLAhEdfJyU/UtFbNkKspcI/AAAAAAAAanY/lEyxTOYqeEA/s1600/D5_Super_Trunfo_Pok%25C3%25A9mon_SlyStalloneBlocky.jpg",
+  atributos:{
+    HP: 700,
+    Attack: 800,
+    Defense: 500,
+    SpAttack: 200,
+    SpDefense: 500,
+    Speed: 200
+}
+}
+
+var cartaD6 = {
+  nome:"Sr. Atros",
+  imagem:"http://1.bp.blogspot.com/-Y3umt30XNNk/UtFbOzn7N5I/AAAAAAAAang/-hMn7gW2Ymc/s1600/D6_Super_Trunfo_Pok%25C3%25A9mon_AtrosAtroce.jpg",
+  atributos:{
+    HP: 600,
+    Attack: 800,
+    Defense: 1000,
+    SpAttack: 500,
+    SpDefense: 500,
+    Speed: 400
+}
+}
+
+var cartaD7 = {
+  nome:"Magnum Von Charge",
+  imagem:"http://3.bp.blogspot.com/-S_OwnfBuLJI/UtFbQTzId6I/AAAAAAAAano/NxMxD_0bxQQ/s1600/D7_Super_Trunfo_Pok%25C3%25A9mon_MagnumVonCharge.jpg",
+  atributos:{
+    HP: 400,
+    Attack: 400,
+    Defense: 900,
+    SpAttack: 600,
+    SpDefense: 900,
+    Speed: 100
+}
+}
+
+var cartaD8 = {
+  nome:"Malbora",
+  imagem:"http://2.bp.blogspot.com/-YTfyWwBvzZM/UtFbRhRU-lI/AAAAAAAAanw/c2K7LdCuRu8/s1600/D8_Super_Trunfo_Pok%25C3%25A9mon_Malbora.jpg",
+  atributos:{
+    HP: 500,
+    Attack: 750,
+    Defense: 750,
+    SpAttack: 300,
+    SpDefense: 500,
+    Speed: 300
+}
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaA1, cartaA2, cartaA3, cartaA4, cartaA5, cartaA6, cartaA7, cartaA8, cartaB1, cartaB2, cartaB3, cartaB4, cartaB5, cartaB6, cartaB7, cartaB8, cartaC1, cartaC2, cartaC3, cartaC4, cartaC5, cartaC6, cartaC7, cartaC8, cartaD1, cartaD2, cartaD3, cartaD4, cartaD5, cartaD6, cartaD7, cartaD8]
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas(){
+  var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+  var html = "Quantidade de cartas no jogo: " + cartas.length
+  
+  divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar(){
+  var divPlacar = document.getElementById('placar')
+  var html = "Jogador " + pontosJogador + "/" + pontosMaquina + " Máquina"
+  
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta(){
+  var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+  cartaMaquina = cartas[numeroCartaMaquina]
+   cartas.splice(numeroCartaMaquina, 1)
+  
+  var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+     cartaJogador = cartas[numeroCartaJogador]
+     cartas.splice(numeroCartaJogador, 1)
+  
+  console.log(cartaJogador)
+//Leon Denis Silvestre de Lucena
+  document.getElementById('btnSortear').disabled = true
+  document.getElementById('btnJogar').disabled = false
+  
+  
+  
+  imagemCartaJogador = document.getElementById("cartaJogador")
+  imagemCartaJogador.src = cartaJogador.imagem
+  var imagemVersos = document.getElementById("versos")
+  imagemVersos.src = "http://c.dx.com/collection/201804/20180410/images/vs.png"
+  imagemCartaMaquina = document.getElementById("cartaMaquina")
+  imagemCartaMaquina.src = "https://i.imgur.com/NxLc6x5.png"
+
+  exibirOpcoes()
+  
+function exibirOpcoes() {
+  var opcoes = document.getElementById('opcoes')
+  var opcoesTexto =""
+  for (var atributo in cartaJogador.atributos){
+    opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "' checked>" + atributo 
+  }
+  opcoes.innerHTML = opcoesTexto
+}
+}
+
+function jogar(){
+  imagemCartaMaquina.src = cartaMaquina.imagem
+  var status = document.getElementById("status")
+  var atributoSelecionado = obtemAtributoSelecionado()
+  if(cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]){
+    status.innerText = "Você Venceu!"
+    pontosJogador++
+    document.getElementById('btnReiniciar').disabled = false 
+      document.getElementById('btnJogar').disabled = true    
+  }
+  else if(cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]){
+    status.innerText = "Você Perdeu!"
+    pontosMaquina++
+      document.getElementById('btnReiniciar').disabled = false
+    document.getElementById('btnJogar').disabled = true
+    
+  }
+  else{status.innerText = "Empate!"}
+    document.getElementById('btnReiniciar').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  
+    if (cartas.length == 0){
+    alert("Fim de Jogo")
+    if(pontosJogador > pontosMaquina){
+      status.innerText = 'Venceu o jogo'
+        document.getElementById('btnReiniciar').disabled = true 
+
+    } else if (pontosMaquina > pontosJogador){
+      status.innerText ='Perdeu o jogo'
+        document.getElementById('btnReiniciar').disabled = true 
+
+    } else {
+      status.innerText = 'O jogo Empatou'
+        document.getElementById('btnReiniciar').disabled = true 
+
+    }
+  } 
+
+  document.getElementById('btnJogar').disabled = true
+  atualizaPlacar()
+  atualizaQuantidadeDeCartas()
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName('atributo')
+  for (var i = 0; i < radioAtributo.length; i++){
+    if (radioAtributo[i].checked){
+      return radioAtributo[i].value
+    }
+  }
+}
+
+function jogarNovamente(){
+  var divStatus = document.getElementById("status")
+  var divCartaJogador = document.getElementById("carta-jogador")
+  var divCartaMaquina = document.getElementById("carta-maquina")
+  var atributoSelecionado = obtemAtributoSelecionado()
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnReiniciar').disabled = true 
+}
+
+//Leon Denis Silvestre de Lucena
